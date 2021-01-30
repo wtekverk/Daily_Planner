1337b4398d19af6e1559118fa3180da8f4ae3e4b
Wendy Tekverk <wtekverk@gmail.com>
2021-01-30 13:05:57 -0700

    changing format so I could get save function to work

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-//EVENT LISTENER 
+//Makes html fully load before JS starts 
 $(document).ready(function () {
 
+    // todays date information      month day year
+    var todaysDate = moment().format('MMM, Do YYYY');
+    $("#currentDay").html(todaysDate);
 
+
+    //EVENT LISTENER 
     $(".saveBtn").on("click", function () {
 
-
+        //
         var time = $(this).attr("id").split("-")[0]
 
         var userInfo = $("#" + time + "-text").val()
@@ -12,41 +17,54 @@ $(document).ready(function () {
         console.log(time, userInfo)
 
         localStorage.setItem(time, userInfo)
-
-
-
     })
 
+    var hour = moment().hour()
 
-    function hourCheck() {
-
-        var hour = moment().hour(); // Number
+    for (var i = 7; i <= 20; i++) {
 
 
-        $("textarea").each(function () {
-
-            
-            var time = parseInt($(this).attr("id").split("-")[0])
-            var text = "#" + time + "-text";
-            
-
-            if (hour > time) {
-                $(text).attr("class", "past")
-            } else if (hour == time) {
-                $(text).attr("class", "present")
-            } else {
-                $(text).attr("class", "future")
-            }
-
-            console.log(text)
-
-        })
+        $("#" + i + "-text").val(localStorage.getItem(i))
+        if (i < hour) {
+            $("#" + i + "-text").addClass("past")
+        } else if (i === hour) {
+            $("#" + i + "-text").addClass("present")
+        } else {
+            $("#" + i + "-text").addClass("future")
+        }
 
 
     }
 
 
-hourCheck()
+    // function hourCheck() {
+
+    //     var hour = moment().hour(); // Number
+
+
+    //     $("textarea").each(function () {
+
+
+    //         var time = parseInt($(this).attr("id").split("-")[0])
+    //         var text = "#" + time + "-text";
+
+    //         if (hour > time) {
+    //             $(text).attr("class", "past")
+    //         } else if (hour == time) {
+    //             $(text).attr("class", "present")
+    //         } else {
+    //             $(text).attr("class", "future")
+    //         }
+
+    //         console.log(text)
+
+    //     })
+
+
+    // }
+
+
+    // hourCheck()
 
 
 
