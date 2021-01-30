01de32d6a77b6d20ffab566747e5a1cfa3547399
Wendy Tekverk <wtekverk@gmail.com>
2021-01-30 00:11:55 -0700

    attaching js to html document

--- script.js
+++ script.js
@@ -3,9 +3,12 @@
 
 $(".saveBtn").on("click", function() {
 
-    
+
     var time = $(this).attr("id").split("-")[0]
+
     var userInfo = $("#" + time + "-text").val()
+
     console.log(time,userInfo)
+    
     localStorage.setItem(time, userInfo)
 })
