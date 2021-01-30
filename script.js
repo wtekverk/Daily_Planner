71c07672494270f8f88560ecddbaf6747ec12ab8
Wendy Tekverk <wtekverk@gmail.com>
2021-01-30 10:11:06 -0700

    made function in JS that conects with actual time to change the color of the text boxes

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
-
 //EVENT LISTENER 
+$(document).ready(function () {
 
-$(".saveBtn").on("click", function() {
+
+    $(".saveBtn").on("click", function () {
 
 
-    var time = $(this).attr("id").split("-")[0]
+        var time = $(this).attr("id").split("-")[0]
 
-    var userInfo = $("#" + time + "-text").val()
+        var userInfo = $("#" + time + "-text").val()
 
-    console.log(time,userInfo)
-    
-    localStorage.setItem(time, userInfo)
+        console.log(time, userInfo)
+
+        localStorage.setItem(time, userInfo)
+
+
+
+    })
+
+
+    function hourCheck() {
+
+        var hour = moment().hour(); // Number
+
+
+        $("textarea").each(function () {
+
+            
+            var time = parseInt($(this).attr("id").split("-")[0])
+            var text = "#" + time + "-text";
+            
+
+            if (hour > time) {
+                $(text).attr("class", "past")
+            } else if (hour == time) {
+                $(text).attr("class", "present")
+            } else {
+                $(text).attr("class", "future")
+            }
+
+            console.log(text)
+
+        })
+
+
+    }
+
+
+hourCheck()
+
+
+
 })
