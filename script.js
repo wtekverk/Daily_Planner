4a931f3462ffa4336efbf9a31e8e261c091aa2b4
Wendy Tekverk <wtekverk@gmail.com>
2021-01-30 00:03:30 -0700

    added click event to JS file for clicking save button

---
+++ script.js
@@ -0,0 +1,6 @@
+$(".saveBtn").on("click", function() {
+    var time = $(this).attr("id").split("-")[0]
+    var userInfo = $("#" + time + "-text").val()
+    console.log(time,userInfo)
+    localStorage.setItem(time, userInfo)
+})
